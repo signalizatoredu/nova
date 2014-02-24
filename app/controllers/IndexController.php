00650646468a3cd563698c7710ee2223bc031fb6
Simon Jonsson <simonpierrejonsson@gmail.com>
2014-02-24 13:32:50 +0100

    Empty IndexController

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -2,15 +2,11 @@
 
 namespace Controllers
 {
-    use Models\Movie;
-
     class IndexController extends ControllerBase
     {
-
         public function indexAction()
         {
-            $movies = Movie::find();
-            $this->view->setVar("movies", $movies);
+
         }
     }
 }
