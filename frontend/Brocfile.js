8a1aa4ea36a9580ff2ff0207c3cff9a64efa4b52
Simon Jonsson <simonpierrejonsson@gmail.com>
2014-07-21 12:36:14 +0200

    add normalize.css to bower and remove kube

--- frontend/Brocfile.js
+++ frontend/Brocfile.js
@@ -17,4 +17,7 @@ var app = new EmberApp();
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
+// Import CSS
+app.import('vendor/normalize-css/normalize.css');
+
 module.exports = app.toTree();
